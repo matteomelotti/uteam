24c515c077c17f62976757fd17282001f3f48adb
Matteo Melotti <matteo.melotti11@gmail.com>
2022-04-05 22:45:18 +0200

    calculateTime function

--- frontend/src/libs/utils.js
+++ frontend/src/libs/utils.js
@@ -1,5 +1,30 @@
+import moment from 'moment'
+import Moment from 'react-moment'
+
 const formatMoney = (locale, currency = 'EUR', number) => {
   return new Intl.NumberFormat('de-DE', { style: 'currency', currency: currency }).format(number)
 }
 
-export { formatMoney }
+const calculateTime = createdAt => {
+  const today = moment(Date.now())
+  const postDate = moment(createdAt)
+  const diffInHours = today.diff(postDate, 'hours')
+
+  if (diffInHours < 24) {
+    return (
+      <>
+        Today <Moment format='hh:mm A'>{createdAt}</Moment>
+      </>
+    )
+  } else if (diffInHours > 24 && diffInHours < 36) {
+    return (
+      <>
+        Yesterday <Moment format='hh:mm A'>{createdAt}</Moment>
+      </>
+    )
+  } else if (diffInHours > 36) {
+    return <Moment format='DD/MM/YYYY hh:mm A'>{createdAt}</Moment>;
+  }
+}
+
+export { formatMoney, calculateTime }
